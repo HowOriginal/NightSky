804b3c95eecd3c6f5ace31d063a08a4f04107445
alibi <ali.bischak@gmail.com>
2013-11-14 18:15:08 -0500

    Update SecondActivity.java

--- TitleScreen/src/com/example/titlescreen/SecondActivity.java
+++ TitleScreen/src/com/example/titlescreen/SecondActivity.java
@@ -1,38 +1,21 @@
 package com.example.titlescreen;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTabHost;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.app.Activity;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentTabHost;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.view.ViewGroup;
 //import android.widget.TextView;
 
-public class SecondActivity extends Fragment {
+public class SecondActivity extends Activity {
 
-    private static View mView;
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
 
-    public static final SecondActivity newInstance(String sampleText) {
-        SecondActivity f = new SecondActivity();
-
-        Bundle b = new Bundle();
-        b.putString("bString", sampleText);
-        f.setArguments(b);
-
-    return f;
+        setContentView(R.layout.second_activity);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        mView = inflater.inflate(R.layout.second_layout, container, false);
-        //String sampleText = getArguments().getString("bString");
-        
-        FragmentTabHost mTabHost;
-        
-        mTabHost = (FragmentTabHost)mView.findViewById(android.R.id.tabhost);
-        mTabHost.setup(getActivity(), getChildFragmentManager(), android.R.id.tabcontent);
-        
-        return mView;
-    }
 }
