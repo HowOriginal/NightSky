71ce744482bf443c8762bbf0d3479168a636ac3b
alibi <ali.bischak@gmail.com>
2013-11-14 18:11:28 -0500

    Update EditStoryScreen.java

--- TitleScreen/src/com/example/titlescreen/EditStoryScreen.java
+++ TitleScreen/src/com/example/titlescreen/EditStoryScreen.java
@@ -1,42 +1,21 @@
 package com.example.titlescreen;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+//import android.support.v4.app.Fragment;
 //import android.support.v4.app.FragmentTabHost;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
 //import android.widget.TextView;
 
-public class EditStoryScreen extends Fragment {
+public class EditStoryScreen extends Activity {
 
-    private static View mView;
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
 
-    public static final EditStoryScreen newInstance(String sampleText) {
-        EditStoryScreen f = new EditStoryScreen();
-
-        Bundle b = new Bundle();
-        b.putString("bString", sampleText);
-        f.setArguments(b);
-
-        
-    return f;
+        setContentView(R.layout.edit_screen);
     }
-    
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        mView = inflater.inflate(R.layout.edit_screen, container, false);
-        //String sampleText = getArguments().getString("bString");
-        
-       /* FragmentTabHost mTabHost;
-        
-        mTabHost = (FragmentTabHost)mView.findViewById(android.R.id.tabhost);
-        mTabHost.setup(getActivity(), getChildFragmentManager(), android.R.id.tabcontent);*/
-        
-        return mView;
-        
-
-    }
 }
