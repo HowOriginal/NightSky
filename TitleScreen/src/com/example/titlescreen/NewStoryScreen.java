f573864c9183fd1734aef3bb3b7d97633fb9c7c3
alibi <ali.bischak@gmail.com>
2013-11-14 18:15:42 -0500

    Create NewStoryScreen.java

---
+++ TitleScreen/src/com/example/titlescreen/NewStoryScreen.java
@@ -0,0 +1,22 @@
+package com.example.titlescreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentTabHost;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+
+public class NewStoryScreen extends Activity {
+
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+
+        setContentView(R.layout.new_screen);
+    
+    //upon clicking button, add story to database
+    }
+    
+}
