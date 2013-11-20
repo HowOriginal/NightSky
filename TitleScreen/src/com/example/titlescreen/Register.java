81deae0c11640398f310030c59fb91a17cd559ba
Eric <ezhang2008@gmail.com>
2013-11-19 23:11:17 -0500

    Changes to final
    addstory, readstory, and login fixes

--- TitleScreen/src/com/example/titlescreen/Register.java
+++ TitleScreen/src/com/example/titlescreen/Register.java
@@ -17,104 +17,104 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Register extends Activity implements OnClickListener{
-	
+public class Register extends Activity implements OnClickListener {
+
 	private EditText user, pass;
-	private Button  mRegister;
-	
-	 // Progress Dialog
-    private ProgressDialog pDialog;
- 
-    // JSON parser class
-    JSONParser jsonParser = new JSONParser();
-    
-    private static final String LOGIN_URL = "http://ezhang.myrpi.org/register.php";
-    
-    private static final String TAG_SUCCESS = "success";
-    private static final String TAG_MESSAGE = "message";
-	
+	private Button mRegister;
+
+	// Progress Dialog
+	private ProgressDialog pDialog;
+
+	// JSON parser class
+	JSONParser jsonParser = new JSONParser();
+
+	private static final String LOGIN_URL = "http://ezhang.myrpi.org/register.php";
+
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_MESSAGE = "message";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register);
-		
-		user = (EditText)findViewById(R.id.username);
-		pass = (EditText)findViewById(R.id.password);
-		
 
-		mRegister = (Button)findViewById(R.id.register);
+		user = (EditText) findViewById(R.id.username);
+		pass = (EditText) findViewById(R.id.password);
+
+		mRegister = (Button) findViewById(R.id.register);
 		mRegister.setOnClickListener(this);
-		
+
 	}
 
 	@Override
 	public void onClick(View v) {
-				new CreateUser().execute();
+		new CreateUser().execute();
 	}
-	
+
 	class CreateUser extends AsyncTask<String, String, String> {
 
 		boolean failure = false;
-		
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(Register.this);
-            pDialog.setMessage("Creating User...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(true);
-            pDialog.show();
-        }
-		
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(Register.this);
+			pDialog.setMessage("Creating User...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+		}
+
 		@Override
 		protected String doInBackground(String... args) {
 			// TODO Auto-generated method stub
-			 // Check for success tag
-            int success;
-            String username = user.getText().toString();
-            String password = pass.getText().toString();
-            try {
-                List<NameValuePair> params = new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("username", username));
-                params.add(new BasicNameValuePair("password", password));
- 
-                Log.d("request!", "starting");
-                
-                //Posting user data to script 
-                JSONObject json = jsonParser.makeHttpRequest(
-                       LOGIN_URL, "POST", params);
- 
-                // full json response
-                Log.d("Login attempt", json.toString());
- 
-                // json success element
-                success = json.getInt(TAG_SUCCESS);
-                if (success == 1) {
-                	Log.d("User Created!", json.toString());              	
-                	finish();
-                	return json.getString(TAG_MESSAGE);
-                }else{
-                	Log.d("Login Failure!", json.getString(TAG_MESSAGE));
-                	return json.getString(TAG_MESSAGE);
-                	
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
- 
-            return null;
-			
+			// Check for success tag
+			int success;
+			String username = user.getText().toString();
+			String password = pass.getText().toString();
+			try {
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("username", username));
+				params.add(new BasicNameValuePair("password", password));
+
+				Log.d("request!", "starting");
+
+				// Posting user data to script
+				JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL, "POST",
+						params);
+
+				// full json response
+				Log.d("Login attempt", json.toString());
+
+				// json success element
+				success = json.getInt(TAG_SUCCESS);
+				if (success == 1) {
+					Log.d("User Created!", json.toString());
+					finish();
+					return json.getString(TAG_MESSAGE);
+				} else {
+					Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+					return json.getString(TAG_MESSAGE);
+
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			return null;
+
 		}
-        protected void onPostExecute(String file_url) {
-            pDialog.dismiss();
-            if (file_url != null){
-            	Toast.makeText(Register.this, file_url, Toast.LENGTH_LONG).show();
-            }
- 
-        }
-		
+
+		protected void onPostExecute(String file_url) {
+			pDialog.dismiss();
+			if (file_url != null) {
+				Toast.makeText(Register.this, file_url, Toast.LENGTH_LONG)
+						.show();
+			}
+
+		}
+
 	}
-		 
 
 }
