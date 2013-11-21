ee1ec0b233a0ea8435c489e05a8394992d67c1d9
Eric <ezhang2008@gmail.com>
2013-11-21 12:15:36 -0500

    GetStory working
    Gets a single story from an id, just replace hard coded id with passed
    in variable

---
+++ TitleScreen/src/com/example/titlescreen/EditStory.java
@@ -0,0 +1,14 @@
+package com.example.titlescreen;
+
+public class TableValues
+{
+	public boolean isUsed;
+	public int xrot;
+	public int yrot;
+	TableValues(int x, int y, boolean used)
+	{
+		xrot = x;
+		yrot = y;
+		isUsed = used;
+	}
+}
