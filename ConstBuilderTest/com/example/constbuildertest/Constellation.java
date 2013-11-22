b91ad390436282defea504a89a71972dc1c32d97
John Ellersick <ellerj@rpi.edu>
2013-11-21 19:20:59 -0500

    Just making sure command line works

---
+++ ConstBuilderTest/com/example/constbuildertest/Constellation.java
@@ -0,0 +1,58 @@
+package com.example.constbuildertest;
+
+
+import java.util.ArrayList;
+
+public class Constellation {
+	
+	private String id;
+	public ArrayList<FloatPair> stars = new ArrayList<FloatPair>();
+	public ArrayList<IntPair> lines = new ArrayList<IntPair>();
+	
+	//Constructors
+	public Constellation() {
+		id = "No id";
+	}
+	public Constellation(String aid) {
+		id = aid;
+	}
+	
+	//Get functions
+	public String getId(){return id;}
+	public FloatPair getStar(int index) {return stars.get(index);}
+	public int numStars() {return stars.size();}
+	public ArrayList<IntPair> getLines() {return lines;}
+	public IntPair getLine(int index) {return lines.get(index);}
+	public int numLines() {return lines.size();}
+	
+	//Set functions
+	public void setId(String aid) {id=aid;}
+	public void setStar(int index, FloatPair astar) { stars.set(index, astar);}
+	public void setLine(int index, IntPair aline) { lines.set(index, aline);}
+	
+	//Add and Delete functions
+	public void addStar(FloatPair astar) {stars.add(astar);}
+	public void deleteStar(int index) {
+		IntPair ip;
+		for (int i=0; i<lines.size(); ++i) {
+			ip = lines.get(i);
+			if (ip.first==index || ip.second==index) {
+				lines.remove(i);
+				--i;
+			}
+		}
+		stars.remove(index);
+		for (int i=0; i<lines.size(); ++i) {
+			ip = lines.get(i);
+			if (ip.first>index) {ip.first-=1;}
+			if (ip.second>index) {ip.second-=1;}
+		}
+	}
+	public void addLine(IntPair aline) {lines.add(aline);}
+	public void deleteLine(int index) {lines.remove(index);}
+	
+	//Draw function
+	public void draw() {
+		//Need to implement.
+	}
+}
---
+++ ConstBuilderTest/com/example/constbuildertest/FloatPair.java
@@ -0,0 +1,11 @@
+package com.example.constbuildertest;
+
+public class FloatPair {
+	public float first;
+	public float second;
+	
+	public FloatPair(float afirst, float asecond) {
+		first = afirst;
+		second = asecond;
+	}
+}
