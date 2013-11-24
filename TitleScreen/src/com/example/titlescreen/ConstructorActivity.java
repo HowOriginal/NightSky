5608397e9ae7aa3f3a6858f65ee324b3e4629e4c
Eric <ezhang2008@gmail.com>
2013-11-24 16:33:26 -0500

    const activity
    made so that it returns to home after saving the new constellation

--- TitleScreen/src/com/example/titlescreen/ConstructorActivity.java
+++ TitleScreen/src/com/example/titlescreen/ConstructorActivity.java
@@ -1,113 +1,115 @@
 package com.example.titlescreen;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
-import android.app.Activity;
 import android.app.ActionBar.LayoutParams;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.Toast;
-
 
 @SuppressLint("InlinedApi")
 public class ConstructorActivity extends Activity {
 
 	private ConstructorView cv;
-	
+	// url for php file
 	String LOGIN_URL = "http://ezhang.myrpi.org/addconstellation.php";
 	String TAG_SUCCESS = "success";
 	String TAG_MESSAGE = "message";
 	JSONParser jsonParser = new JSONParser();
 	ProgressDialog pDialog;
-	
-	
-	
+
 	@SuppressLint("InlinedApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		RelativeLayout rl = new RelativeLayout(this);
-		
+
 		cv = new ConstructorView(this);
-		cv.setBackgroundColor(Color.rgb(10,10,45));
+		cv.setBackgroundColor(Color.rgb(10, 10, 45));
 		rl.addView(cv);
-        
-                ActionBar actionBar = getActionBar(); //getSupportActionBar()
+
+		ActionBar actionBar = getActionBar(); // getSupportActionBar()
 		actionBar.setDisplayHomeAsUpEnabled(true);
-		
 
-	    RelativeLayout.LayoutParams bp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		RelativeLayout.LayoutParams bp = new RelativeLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 
-	    
-	    Button savebutton = new Button(this);
-	    bp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-	    bp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-	    bp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-	    savebutton.setLayoutParams(bp);
-	    savebutton.setText("Save");
-	    savebutton.setOnClickListener(new View.OnClickListener() {
-	        @Override
-	        public void onClick(View v) {
-	        	//Retrieve the Constellation from the Constructor.
-	        	Constellation con = cv.getConstellation();
-	        	
-	        	Create temp = new Create();
-	        	temp.s = con.stars;
-	        	temp.l = con.lines;
-	        	
-	        	if (temp.s.size() < 1) {
-	        		Context context = getApplicationContext();
+		Button savebutton = new Button(this);
+		bp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
+		bp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		bp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		savebutton.setLayoutParams(bp);
+		savebutton.setText("Save");
+		savebutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Retrieve the Constellation from the Constructor.
+				Constellation con = cv.getConstellation();
+
+				Create temp = new Create();
+				temp.s = con.stars;
+				temp.l = con.lines;
+
+				if (temp.s.size() < 1) {
+					Context context = getApplicationContext();
 					int duration = Toast.LENGTH_LONG;
-					Toast toas = Toast.makeText(context, "Need at least 1 star to save.", duration);
+					Toast toas = Toast.makeText(context,
+							"Need at least 1 star to save.", duration);
 					toas.show();
-	        		return;
-	        	}
-	        	
-	        	temp.execute();
-	            
-	        }
-	    });
-	    
-	    Button clearbutton = new Button(this);
-	    bp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-	    bp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-	    bp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-	    clearbutton.setLayoutParams(bp);
-	    clearbutton.setText("Clear");
-	    clearbutton.setOnClickListener(new View.OnClickListener() {
-	        @Override
-	        public void onClick(View v) {
-	        	cv.setConstellation(new Constellation());
-	        }
-	    });
-	    
-	    rl.addView(clearbutton);
-	    rl.addView(savebutton);
-	    
-	    setContentView(rl);
+					return;
+				}
+
+				temp.execute();
+				Intent j = new Intent(ConstructorActivity.this, LaunchScreen.class);
+				startActivity(j);
+			}
+		});
+
+		Button clearbutton = new Button(this);
+		bp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
+		bp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		bp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+		clearbutton.setLayoutParams(bp);
+		clearbutton.setText("Clear");
+		clearbutton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				cv.setConstellation(new Constellation());
+
+			}
+		});
+
+		rl.addView(clearbutton);
+		rl.addView(savebutton);
+
+		setContentView(rl);
 	}
 
-	
 	class Create extends AsyncTask<String, String, String> {
-		
+
 		ArrayList<FloatPair> s;
 		ArrayList<IntPair> l;
-		
+
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
@@ -123,80 +125,59 @@ public class ConstructorActivity extends Activity {
 			// TODO Auto-generated method stub
 			// Check for success tag
 			// int success;
-			
+
 			try {
-				String[] starfieldnames = {"x1","x2","x3","x4","x5","x6","x7","y1","y2","y3","y4","y5","y6","y7"};
-				String[] linefieldnames = {"p1a","p1b","p2a","p2b","p3a","p3b","p4a","p4b","p5a","p5b","p6a","p6b","p7a","p7b"};
+				String[] starfieldnames = { "x1", "x2", "x3", "x4", "x5", "x6",
+						"x7", "y1", "y2", "y3", "y4", "y5", "y6", "y7" };
+				String[] linefieldnames = { "p1a", "p1b", "p2a", "p2b", "p3a",
+						"p3b", "p4a", "p4b", "p5a", "p5b", "p6a", "p6b", "p7a",
+						"p7b" };
 				List<NameValuePair> params = new ArrayList<NameValuePair>();
-				
-				for (int i=0; i<7; ++i) {
-					if (s.size()>i) {
-						params.add(new BasicNameValuePair(starfieldnames[i], Float.toString(s.get(i).first)));
-						params.add(new BasicNameValuePair(starfieldnames[i+7], Float.toString(s.get(i).second)));
+
+				for (int i = 0; i < 7; ++i) {
+					if (s.size() > i) {
+						params.add(new BasicNameValuePair(starfieldnames[i],
+								Float.toString(s.get(i).first)));
+						params.add(new BasicNameValuePair(
+								starfieldnames[i + 7],
+								Float.toString(s.get(i).second)));
+					} else {
+						params.add(new BasicNameValuePair(starfieldnames[i],
+								Float.toString(s.get(0).first)));
+						params.add(new BasicNameValuePair(
+								starfieldnames[i + 7],
+								Float.toString(s.get(0).second)));
 					}
-					else {
-						params.add(new BasicNameValuePair(starfieldnames[i], Float.toString(s.get(0).first)));
-						params.add(new BasicNameValuePair(starfieldnames[i+7], Float.toString(s.get(0).second)));
-					}
-					if (l.size()>i) {
-						params.add(new BasicNameValuePair(linefieldnames[2*i], Integer.toString(l.get(i).first)));
-						params.add(new BasicNameValuePair(linefieldnames[2*i+1], Integer.toString(l.get(i).second)));
-					}
-					else {
-						params.add(new BasicNameValuePair(linefieldnames[2*i], "0"));
-						params.add(new BasicNameValuePair(linefieldnames[2*i+1], "0"));
+					if (l.size() > i) {
+						params.add(new BasicNameValuePair(
+								linefieldnames[2 * i], Integer.toString(l
+										.get(i).first)));
+						params.add(new BasicNameValuePair(
+								linefieldnames[2 * i + 1], Integer.toString(l
+										.get(i).second)));
+					} else {
+						params.add(new BasicNameValuePair(
+								linefieldnames[2 * i], "0"));
+						params.add(new BasicNameValuePair(
+								linefieldnames[2 * i + 1], "0"));
 					}
 				}
-				/*
-				params.add(new BasicNameValuePair("x1", Float.toString(s.get(0).first)));
-				params.add(new BasicNameValuePair("x2", Float.toString(s.get(1).first)));
-				params.add(new BasicNameValuePair("x3", Float.toString(s.get(2).first)));
-				params.add(new BasicNameValuePair("x4", Float.toString(s.get(3).first)));
-				params.add(new BasicNameValuePair("x5", Float.toString(s.get(4).first)));
-				params.add(new BasicNameValuePair("x6", Float.toString(s.get(5).first)));
-				params.add(new BasicNameValuePair("x7", Float.toString(s.get(6).first)));
-				params.add(new BasicNameValuePair("y1", Float.toString(s.get(0).second)));
-				params.add(new BasicNameValuePair("y2", Float.toString(s.get(1).second)));
-				params.add(new BasicNameValuePair("y3", Float.toString(s.get(2).second)));
-				params.add(new BasicNameValuePair("y4", Float.toString(s.get(3).second)));
-				params.add(new BasicNameValuePair("y5", Float.toString(s.get(4).second)));
-				params.add(new BasicNameValuePair("y6", Float.toString(s.get(5).second)));
-				params.add(new BasicNameValuePair("y7", Float.toString(s.get(6).second)));
-				
-				params.add(new BasicNameValuePair("p1a", Integer.toString(l.get(0).first)));
-				params.add(new BasicNameValuePair("p1b", Integer.toString(l.get(0).second)));
-				params.add(new BasicNameValuePair("p2a", Integer.toString(l.get(1).first)));
-				params.add(new BasicNameValuePair("p2b", Integer.toString(l.get(1).second)));
-				params.add(new BasicNameValuePair("p3a", Integer.toString(l.get(2).first)));
-				params.add(new BasicNameValuePair("p3b", Integer.toString(l.get(2).second)));
-				params.add(new BasicNameValuePair("p4a", Integer.toString(l.get(3).first)));
-				params.add(new BasicNameValuePair("p4b", Integer.toString(l.get(3).second)));
-				params.add(new BasicNameValuePair("p5a", Integer.toString(l.get(4).first)));
-				params.add(new BasicNameValuePair("p5b", Integer.toString(l.get(4).second)));
-				params.add(new BasicNameValuePair("p6a", Integer.toString(l.get(5).first)));
-				params.add(new BasicNameValuePair("p6b", Integer.toString(l.get(5).second)));
-				params.add(new BasicNameValuePair("p7a", Integer.toString(l.get(6).first)));
-				params.add(new BasicNameValuePair("p7b", Integer.toString(l.get(6).second)));
-				*/
-				
+
 				Log.d("request!", "starting");
-				pDialog.setMessage(Integer.toString(params.size()));
-				
+				pDialog.setMessage("Saving");
+
 				// Posting user data to script
 				jsonParser.makeHttpRequest(LOGIN_URL, "POST", params);
 
 				// full json response
-				/*Log.d("Add attempt", json.toString());
-				success = 1;
-				if (success == 1) {
-					Log.d("Added!", json.toString());
-					finish();
-				} else {
-					Log.d("Addition Failed!", json.getString(TAG_MESSAGE));
-				}*/
-			}finally
-			{
-				
+				/*
+				 * Log.d("Add attempt", json.toString()); success = 1; if
+				 * (success == 1) { Log.d("Added!", json.toString()); finish();
+				 * } else { Log.d("Addition Failed!",
+				 * json.getString(TAG_MESSAGE)); }
+				 */
+			} finally {
+
 			}
 
 			return null;
@@ -211,16 +192,16 @@ public class ConstructorActivity extends Activity {
 			}
 		}
 	}
-	
-		 @Override
-	    public boolean onOptionsItemSelected(MenuItem item) {
-	        switch (item.getItemId()) {
-	        case android.R.id.home:
-	            this.finish();
-	            return true;
-	        }
-	        return super.onOptionsItemSelected(item);
-	    }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			this.finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -228,5 +209,5 @@ public class ConstructorActivity extends Activity {
 		getMenuInflater().inflate(R.menu.launch_screen, menu);
 		return true;
 	}
-	
+
 }
